c9e658d4dbf13807be16ba83be16123c9abc7500
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-01-01 22:38:35 +0800

    Create test_stats.py

---
+++ test_stats.py
@@ -0,0 +1,9 @@
+import stats 
+
+listX = [10, 20, 20, 34, 31, 44, 50]
+
+print("Testing stats module...")
+print("List:", listX)
+print("Mean   :", stats.mean(listX))
+print("Median :", stats.median(listX))
+print("Mode   :", stats.mode(listX))
